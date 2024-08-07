3c2479e33b85547280d5f1fb0eba45fc0915b58e
Jack Webdev <jackbuda31@gmail.com>
2024-08-07 05:19:19 +0200

    breadcrumbs

--- src/app/(web)/admin/employees/add-employee/page.tsx
+++ src/app/(web)/admin/employees/add-employee/page.tsx
@@ -1,11 +1,33 @@
 "use client";
 import { ArrowLeft } from "lucide-react";
 import { useRouter } from "next/navigation";
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+} from "@/packages/ui/breadcrumb";
+import Link from "next/link";
 export default function AddEmployee() {
   const router = useRouter();
   return (
     <div>
-      <ArrowLeft onClick={() => router.back()} className="text-black cursor-pointer" />
+            <Breadcrumb>
+        <BreadcrumbList>
+          <BreadcrumbItem>
+            <BreadcrumbLink>
+              <Link href="/admin/employees">All Employees</Link>
+            </BreadcrumbLink>
+          </BreadcrumbItem>
+          <BreadcrumbSeparator />
+          <BreadcrumbItem>
+            <BreadcrumbPage>Add New Employee</BreadcrumbPage>
+          </BreadcrumbItem>
+        </BreadcrumbList>
+      </Breadcrumb>
+      {/* <ArrowLeft onClick={() => router.back()} className="text-black cursor-pointer" /> */}
       <h1>Add New Employee</h1>
     </div>
   )
